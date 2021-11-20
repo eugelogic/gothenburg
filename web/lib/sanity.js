7d82aca356ccb458c3241c3ed0763aaa25ea85e5
Eugene Molari <eugelogic@gmail.com>
2021-11-20 11:54:19 +0000

    Query and render posts on the homepage

--- web/lib/sanity.js
+++ web/lib/sanity.js
@@ -1,28 +1,19 @@
-// lib/sanity.js
+import { config } from './config'
 import {
-    createImageUrlBuilder,
-    createPortableTextComponent,
-    createPreviewSubscriptionHook,
-    createCurrentUserHook,
+  createClient,
+  createImageUrlBuilder,
+  createPortableTextComponent
   } from 'next-sanity'
-  import {config} from './config'
 
-  /**
-   * Set up a helper function for generating Image URLs with only the asset reference data in your documents.
-   * Read more: https://www.sanity.io/docs/image-url
-   **/
-  export const urlFor = (source) => createImageUrlBuilder(config).image(source)
+if (!config.projectId) {
+  throw Error("No Project ID set. Check your environment variables.");
+}
 
-  // Set up the live preview subscription hook
-  export const usePreviewSubscription = createPreviewSubscriptionHook(config)
+export const sanityClient = createClient(config)
 
-  // Set up Portable Text serialization
-  export const PortableText = createPortableTextComponent({
-    ...config,
-    // Serializers passed to @sanity/block-content-to-react
-    // (https://github.com/sanity-io/block-content-to-react)
-    serializers: {},
-  })
+export const urlFor = (source) => createImageUrlBuilder(config).image(source)
 
-  // Helper function for using the current logged in user account
-  export const useCurrentUser = createCurrentUserHook(config)
+export const PortableText = createPortableTextComponent({
+  ...config,
+  serializers: {}
+})
--- web/pages/index.js
+++ web/pages/index.js
@@ -1,7 +1,35 @@
+import { sanityClient } from '../lib/sanity'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+const postsQuery = `*[_type == 'post']{
+  _id,
+  title,
+  slug,
+  excerpt,
+  author->{
+    name
+  },
+  mainImage,
+  category->{
+    name
+  },
+  publishedAt
+}|order(publishedAt desc)`
+
+export const getStaticProps = async () => {
+
+  const posts = await sanityClient.fetch(postsQuery)
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
+const Home = ({ posts }) => {
+
   return (
     <div className={styles.container}>
       <Head>
@@ -16,8 +44,19 @@ export default function Home() {
           Gothenburg
         </h1>
 
+        <ul>
+          {posts?.length > 0 && posts.map(post =>(
+            <li key={post._id}>
+              <h2>{post.title}</h2>
+            </li>
+          ))}
+
+        </ul>
+
       </main>
 
     </div>
   )
 }
+
+export default Home
