9351ad739ed69400c66a8103d6daeca514b4d70c
Eugene Molari <eugelogic@gmail.com>
2021-11-22 10:21:55 +0000

    Fix env var issue by moving .env.local file inside the Web folder

--- web/lib/config.js
+++ web/lib/config.js
@@ -1,6 +1,5 @@
 export const config = {
-  projectId: 'nilynrmp',
-  // projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   apiVersion: '2021-11-18',
   useCdn: process.env.NODE_ENV === 'production',
