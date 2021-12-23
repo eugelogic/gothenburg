78e871a06a6861627dd3e039efad3e8b13333d62
Eugene Molari <eugelogic@gmail.com>
2021-12-23 10:36:54 +0000

    Add style to blog index cards

--- web/pages/index.js
+++ web/pages/index.js
@@ -64,28 +64,32 @@ const Home = ({ siteSettings, posts }) => {
 
         <ul className="grid gap-5 md:grid-cols-3">
           {items?.length > 0 && items.map(item =>(
-            <li key={item._id}>
+            <li key={item._id} className="flex flex-col rounded-lg shadow-lg overflow-hidden hover:shadow-2xl">
               <Link href={`/blog/${item.slug.current}`}>
                 <a>
                   <Image src={urlFor(item.mainImage).url()} alt={item.mainImage.alt} width={780} height={585}/>
                 </a>
               </Link>
-              <p>{item.category.name}</p>
-              <Link href={`/blog/${item.slug.current}`}>
-                <a>
-                  <h2>{item.title}</h2>
-                  <p>{item.excerpt}</p>
-                </a>
-              </Link>
-              <footer>
-                <div>
-                  <Image src={urlFor(item.author.image).url()} alt={item.author.name} width={50} height={50}/>
-                </div>
-                <div>
-                  <p>{item.author.name}</p>
-                  <time dateTime={item.publishedAt}><FormatDate date={item.publishedAt} /></time>
-                </div>
-              </footer>
+              <p className="uppercase pt-4 pl-4">{item.category.name}</p>
+              <div className="flex-1 flex flex-col justify-between">
+                <Link href={`/blog/${item.slug.current}`}>
+                  <a>
+                    <div className="p-4">
+                      <h2 className="text-xl font-bold">{item.title}</h2>
+                      <p>{item.excerpt}</p>
+                    </div>
+                  </a>
+                </Link>
+                <footer className="flex p-4 flex-shrink-0">
+                  <div>
+                    <Image src={urlFor(item.author.image).url()} alt={item.author.name} width={50} height={50} className="rounded-full"/>
+                  </div>
+                  <div className="ml-4">
+                    <p>{item.author.name}</p>
+                    <time className="text-gray-500" dateTime={item.publishedAt}><FormatDate date={item.publishedAt} /></time>
+                  </div>
+                </footer>
+              </div>
             </li>
           ))}
         </ul>
@@ -94,7 +98,7 @@ const Home = ({ siteSettings, posts }) => {
           items={posts}
           action={action}
           postsPerPage={postsPerPage}
-          className="max-w-[7rem] flex justify-evenly m-auto"
+          className="max-w-[7rem] flex justify-evenly mx-auto my-7"
         />
 
       </main>
