bc5b71e6f12e3a9f89758fc6b2f5456ee6629e14
Eugene Molari <eugelogic@gmail.com>
2022-01-13 15:22:04 +0000

    Add basic styles to the Text Section component

--- web/components/sections/TextSection.js
+++ web/components/sections/TextSection.js
@@ -1,15 +1,26 @@
-import {
-    PortableText,
-} from '../../lib/sanity'
+import PortableText from "react-portable-text";
 
 const TextSection = (props) => {
 
     const { heading, text } = props
 
     return (
-        <div>
-            <h2>{heading}</h2>
-            <PortableText blocks={text} />
+        <div className="max-w-[56rem] mx-auto px-5">
+            <h2 className="mt-3 mb-2 text-3xl font-bold">{heading}</h2>
+                <PortableText
+                    className="w-full"
+                    content={text}
+                    serializers={{
+                        h1: (props) => <h1 className="mt-3 mb-2 text-3xl font-bold uppercase" {...props} />,
+                        h2: (props) => <h2 className="mt-3 mb-2 text-3xl font-bold" {...props} />,
+                        h3: (props) => <h3 className="mt-2 mb-1 text-2xl font-bold" {...props} />,
+                        h4: (props) => <h4 className="mt-2 mb-1 text-xl font-bold" {...props} />,
+                        normal: (props) => <p className="my-2" {...props} />,
+                        link: (props) => <a className="underline hover:no-underline" {...props} />,
+                        ul: (props) => <ul className="my-4 pl-5 list-disc" {...props} />,
+                        blockquote: (props) => <blockquote className="relative p-6 text-l italic border-l-4 bg-neutral-100 text-neutral-600 border-neutral-500 quote" {...props} />
+                    }}
+                />
         </div>
     )
 }
