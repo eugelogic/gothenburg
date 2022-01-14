5278a6e0f8d856190d14b1016f3b0c55be1eb850
Eugene Molari <eugelogic@gmail.com>
2022-01-14 15:28:23 +0000

    Fix style of main nav menu on mobile layout

--- web/components/Header.js
+++ web/components/Header.js
@@ -18,7 +18,7 @@ const Header = ({ siteSettings }) => {
             <nav>
                 <ul className="flex">
                 {mainNavigation?.length > 0 && mainNavigation.map(navItem => (
-                        <li key={navItem._id} className="font-headings text-sm md:text-lg p-4">
+                        <li key={navItem._id} className="font-headings text-sm px-2 py-4 md:text-lg md:p-4">
                             <Link href={`/${navItem.slug.current}`}>
                                 <a>
                                     {navItem.title}
